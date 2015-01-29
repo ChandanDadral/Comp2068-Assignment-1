4084b34ced424da2a973d18f057f01d8bb307a7a
chandanDadral <cdadral9@gmail.com>
2015-01-29 13:33:06 -0500

    Added Whole all the Slides to the game

--- Comp2068-Assignment-1/js/jScript.js
+++ Comp2068-Assignment-1/js/jScript.js
@@ -9,3 +9,22 @@ audio.loop = true;
 audio.onended = function () {
     audio.play();
 }
+
+//Script for Time
+var c=10
+var t 
+function stopCount() 
+{ 
+    clearTimeout(t) 
+} 
+function timedCount()
+{ document.getElementById('txt').value=c 
+c=c-1 
+if(c==-1)
+{
+    alert("time over")
+    stopcount()
+}
+t = setTimeout("timedCount()", 1000)
+}
+
